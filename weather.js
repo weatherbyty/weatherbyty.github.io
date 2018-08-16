9afe312e814292ff56ef84797fa06b08d990aea8
Eric Wuehler <eric.wuehler@mcafee.com>
2018-08-16 15:53:11 -0700

    moved temp to vars

---
+++ weather.js
@@ -0,0 +1,25 @@
+/*
+    This document has all the current data that goes into the main page
+*/
+
+
+var monHigh = 77
+var monLow = 52
+
+var tueHigh = 78
+var tueLow = 50
+
+var wedHigh = 77
+var wedLow = 53
+
+var thuHigh = 75
+var thuLow = 49
+
+var friHigh = 73
+var friLow = 48
+
+
+
+
+
+
