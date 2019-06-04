d5ce14ed5b10b085561901195b54b5dcb20d34c6
Tynan Wuehler <42504698+tywuehler@users.noreply.github.com>
2019-06-03 18:07:19 -0700

    Update weather.js

--- weather.js
+++ weather.js
@@ -54,10 +54,10 @@ var NOTE = 4;
 
 var weekof = 'June 3-7';
 var monday = [ 73, 47, 'Sun', SUNNY[FLAT], '9 days'];
-var tuesday = [ 77, 53, 'SunSunSun', SUNNY[FLAT], '8 days'];
-var wednesday = [ 66, 45, 'Clouds', CLOUDY_[FLAT], ' 1 week!'];
-var thursday = [ 63, 49, 'Rain showers', RAIN_SHOWERS[FLAT], '6 days'];
-var friday = [ 61, 47, 'Rain', RAIN[FLAT], 'Stupider Clouds- 5 days'];
+var tuesday = [ 75, 51, 'SunSunSun', SUNNY[FLAT], '8 days'];
+var wednesday = [ 67, 48, 'Clouds', CLOUDY_[FLAT], ' 1 week!'];
+var thursday = [ 64, 49, 'Rain showers', RAIN_SHOWERS[FLAT], '6 days'];
+var friday = [ 59, 46, 'Thunderstorms', THUNDERSTORM[FLAT], 'Stupider Clouds- 5 days'];
 
 
 
