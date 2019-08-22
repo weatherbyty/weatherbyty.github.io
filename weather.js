6064855b2f041e08fcd5ae6d991ad02b03518923
Tynan Wuehler <42504698+tywuehler@users.noreply.github.com>
2019-08-22 11:18:01 -0700

    AUgust 26-30

--- weather.js
+++ weather.js
@@ -51,12 +51,12 @@ var FORECAST = 2;
 var PICTURE = 3;
 var NOTE = 4;
 
-var weekof = 'August 5-9 Weather for Murrayhill, Beaverton, OR';
-var monday = [ 91, 61, 'Sunny', SUNNY[FLAT], 'HOT'];
-var tuesday = [ 92, 58, 'Sun', SUNNY[FLAT], 'Still HOT'];
-var wednesday = [ 84, 57, '20% chance of rain', SUNNY[FLAT], 'Ok'];
-var thursday = [ 80, 55, 'Partly Cloudy', CLOUDY_PARTLY[FLAT], 'back to normal'];
-var friday = [ 81, 55, 'Finally', CLOUDY_PARTLY[FLAT], 'Phew'];
+var weekof = 'August 26-30 Weather for Beaverton, OR';
+var monday = [ 85, 56, 'Sunny', SUNNY[FLATLINE], '8 Days'];
+var tuesday = [ 90, 62, 'Sun', CLOUDY_PARTLY[FLATLINE], ' HOT'];
+var wednesday = [ 92, 64, '!!!', CLOUDY_PARTLY[FLATLINE], '6 Days'];
+var thursday = [ 89, 61, 'Partly Cloudy', SUNNY[FLATLINE], 'A little better'];
+var friday = [ 88, 57, 'Still hot', CLOUDY_MOSTLY[FLATLINE], 'Ok..'];
 
 
 
